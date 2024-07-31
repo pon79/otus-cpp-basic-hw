8ca372aac28b9a334e23ca38c380e8c1b891a11f
pon79 <pon79job@google.com>
2024-07-31 10:15:16 +0300

    main: use myVector

--- hw_6/main.cpp
+++ hw_6/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 
+#include "sequentialcontainer.h"
+
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    SequentialContainer myVector;
 
+    myVector.push_back( 1 );
+    myVector.push_back( 2 );
 
     return 0;
 }
