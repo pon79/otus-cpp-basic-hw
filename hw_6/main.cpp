da66c89bb5abf866fcdb7cf20d3b745523dfaf84
Aleksandr <pon79a@gmail.com>
2026-05-13 16:20:26 +0200

    SequentialContainer: Convert to template class
    Refactor `SequentialContainer` to be a generic template class, allowing it to hold elements of any type `T`. This enhances its reusability.
    
    The implementation has been moved entirely into the `sequentialcontainer.h` header file, and `sequentialcontainer.cpp` has been removed, aligning with standard C++ practices for template definitions. `main.cpp` is updated to use the templated version.

--- hw_6/main.cpp
+++ hw_6/main.cpp
@@ -22,7 +22,7 @@ int main()
 
 void testSequentialContainer()
 {
-    SequentialContainer myVector = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    SequentialContainer<int> myVector = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
     std::cout << myVector;
 
--- hw_6/sequentialcontainer.h
+++ hw_6/sequentialcontainer.h
@@ -1,34 +1,114 @@
 #ifndef SEQUENTIALCONTAINER_H
 #define SEQUENTIALCONTAINER_H
 
+#include <cstddef>
 #include <iostream>
-#include <cuchar> // size_t
+#include <cuchar>   // size_t
 #include <initializer_list>
+#include <algorithm> // copy
 
+
+template <typename T>
 class SequentialContainer
 {
 public:
-    SequentialContainer();
-    SequentialContainer( size_t n );
-    SequentialContainer( std::initializer_list<int> list);
+    // Конструктор по умолчанию
+    SequentialContainer() : m_region{ nullptr }, m_size{0} {}
 
-    ~SequentialContainer();
+    // Конструктор с заданным размером
+    SequentialContainer(size_t n) : m_region{ new T[n] }, m_size{n} {
+        for( size_t index{0}; index < m_size ; index++) {
+            m_region[index] = T{};
+        }
+    }
 
-    void push_back( int value );
-    void erase(const int eraseIndex );
-    void insert(const int insertIndex , const int value);
+    // Конструктор с инициализацией списка
+    SequentialContainer(std::initializer_list<T> list)
+        : m_region{ new T[list.size()] }
+        , m_size{list.size()}
+    {
+        std::copy(list.begin(), list.end(), m_region);
+    }
 
-    size_t size() const;
+    ~SequentialContainer() { delete[] m_region; }
 
-    int& operator[](const int index);
-    int operator[](const int index) const;
+    void push_back( const T& value )
+    {
+        T* new_region = new T[m_size + 1];
+
+        for (size_t i = 0; i < m_size; ++i) {
+            new_region[i] = m_region[i];
+        }
+
+        new_region[m_size] = value;
+        delete [] m_region;
+        m_region = new_region;
+        m_size += 1;
+    }
+
+    void erase(size_t eraseIndex)
+    {
+        if( eraseIndex >= m_size ) {
+            return;
+        }
+
+        T* new_region = new T[m_size - 1];
+
+        for (size_t index = 0; index < eraseIndex; ++index) {
+            new_region[index] = m_region[index];
+        }
+
+        for (size_t index = eraseIndex + 1; index < m_size; ++index) {
+            new_region[index - 1] = m_region[index];
+        }
+
+        delete [] m_region;
+        m_region = new_region;
+        m_size -= 1;
+    }
+        
+    void insert(size_t insertIndex , const T& value)
+    {
+        if( insertIndex > m_size ) {
+            return;
+        }
+
+        T* new_region = new T[m_size + 1];
+
+        for (size_t index = 0; index < insertIndex; ++index) {
+            new_region[index] = m_region[index];
+        }
+
+        new_region[insertIndex] = value;
+
+        for (size_t index = insertIndex; index < m_size; ++index) {
+            new_region[index + 1] = m_region[index];
+        }
+
+        delete [] m_region;
+        m_region = new_region;
+        m_size += 1;
+    }
+
+    size_t size() const { return m_size; }
+
+    T& operator[](size_t index)
+    {
+        return m_region[index]; // TO DO check index
+    }
+
+    const T& operator[](size_t index) const
+    {
+        return m_region[index]; // TO DO check index
+    }
 
 private:
-    int *m_region;
+    T *m_region;
     size_t m_size;
 };
 
-inline std::ostream& operator<<( std::ostream& stream, SequentialContainer &container)
+template <typename T>
+inline std::ostream& operator<<( std::ostream& stream, SequentialContainer<T> &container)
 {
     for( std::size_t index{0}; index < container.size() ; index++ ) {
         std::cout << container[index];
